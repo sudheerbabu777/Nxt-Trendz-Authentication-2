523b2ed5d82776c0ccd3e931099eb83eaef7dbc5
sudheerbabu777 <you@example.comthummalasudheerbabu74@gmail.com>
2023-02-07 17:42:15 +0000

    Nxt Trendz Authentication 2

---
+++ src/components/Cart/index.js
@@ -0,0 +1,27 @@
+// Write your JS code here
+import {Redirect} from 'react-router-dom'
+import Cookies from 'js-cookie'
+import Header from '../Header'
+import './index.css'
+
+const Cart = () => {
+  const jwtToken = Cookies.get('jwt_token')
+  if (jwtToken === undefined) {
+    return <Redirect to="/login" />
+  }
+  return (
+    <div>
+      <Header />
+
+      <div className="cart-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+          alt="cart"
+          className="cart-image"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Cart
